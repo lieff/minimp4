a7632d08e8033c2fc9fd081ea4aaf6c143a9a5e8
lieff <lieff@users.noreply.github.com>
2019-12-12 14:48:21 +0300

    add h264 with slices test

--- scripts/test.sh
+++ scripts/test.sh
@@ -11,6 +11,15 @@ then
     exit 1
 fi
 rm out.mp4
+
+./minimp4_x86 vectors/foreman_slices.264 out.mp4
+if ! cmp ./out.mp4 vectors/out_slices_ref.mp4 >/dev/null 2>&1
+then
+    echo test failed
+    exit 1
+fi
+rm out.mp4
+
 qemu-arm ./minimp4_arm_gcc vectors/foreman.264 out.mp4
 if ! cmp ./out.mp4 vectors/out_ref.mp4 >/dev/null 2>&1
 then
@@ -18,4 +27,13 @@ then
     exit 1
 fi
 rm out.mp4
+
+qemu-arm ./minimp4_arm_gcc vectors/foreman_slices.264 out.mp4
+if ! cmp ./out.mp4 vectors/out_slices_ref.mp4 >/dev/null 2>&1
+then
+    echo test failed
+    exit 1
+fi
+rm out.mp4
+
 echo test passed
